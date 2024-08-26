17afee4d78a42b2b8ee5dd347add445158f4e52b
Diogo Calmon <99301592+DiogoCalmon@users.noreply.github.com>
2024-08-26 10:13:37 -0300

    adicionando js

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ function atualizarBotao() {
     const larguraTelaModelo = 720
     const larguraTelaAtual = window.innerWidth
 
-    
+    const larguraBotao = Math.max(200, larguraTelaAtual - larguraTelaModelo)
+
+    document.getElementsByClassName("ball").style.width = `${larguraBotao}px`
 }
+
+document.addEventListener('DOMContentLoaded', atualizarBotao);
+
+window.addEventListener('resize', atualizarBotao)
