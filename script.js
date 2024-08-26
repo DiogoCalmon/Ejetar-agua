806ebc223866912cb9f8e8fe7684eba01e7a257a
Diogo Calmon <99301592+DiogoCalmon@users.noreply.github.com>
2024-08-26 10:23:58 -0300

    solving issues

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function atualizarBotao() {
-    const larguraTelaModelo = 720
-    const larguraTelaAtual = window.innerWidth
+    const larguraTelaModelo = 720;
+    const larguraTelaAtual = window.innerWidth;
 
-    const larguraBotao = Math.max(200, larguraTelaAtual - larguraTelaModelo)
+    const comprimentoTelaModelo = 540;
+    const comprimentoTelaAtual = window.innerHeight
 
-    document.getElementsByClassName("ball").style.width = `${larguraBotao}px`
+
+    const larguraBotao = Math.max(200, larguraTelaAtual - larguraTelaModelo);
+    const comprimentoBotao = Math.max(200, comprimentoTelaAtual - comprimentoTelaModelo);
+
+    document.querySelector('.ball').style.width = `${larguraBotao}px`;
+    document.querySelector('.ball').style.height = `${comprimentoBotao}px`;
 }
 
 document.addEventListener('DOMContentLoaded', atualizarBotao);
 
-window.addEventListener('resize', atualizarBotao)
+window.addEventListener('resize', atualizarBotao);
