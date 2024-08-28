d2ae7c75bbd54d130f632ec4393ba306a0ab7e2e
Diogo Calmon <99301592+DiogoCalmon@users.noreply.github.com>
2024-08-28 11:13:56 -0300

    almost ready

--- script.js
+++ script.js
@@ -15,15 +15,27 @@ window.addEventListener("load", () => {
         console.log("viewportHeight: " + viewportHeight);
         console.log("currentHeightVh: " + currentHeightVh);
 
+        const text = document.querySelector(".texts");
+        
+        const currentTextPx = parseFloat(window.getComputedStyle(text).height);
+        const viewportText = window.innerHeight;
+        const currentTextVh = (currentTextPx / viewportText) * 100;
+
         if (currentHeightVh > 10)
         {
             header.style.opacity = '0%';
             header.style.height = '3vh';
+
+            text.style.opacity = '100%';
+            text.style.height = '0%';
         }
         else
         {
             header.style.opacity = '100%';
             header.style.height = '20vh';
+
+            text.style.opacity = '0%';
+            text.style.height = '50%';
         }
         
         icon.classList.toggle("playing");
